2aa946756fc2939a91d8acaaa29a53b5b18cb072
galperins4 <galperins4@gmail.com>
2018-06-14 23:54:50 -0500

    Add files via upload

--- park/one/vote.py
+++ park/one/vote.py
@@ -4,7 +4,7 @@ from park.one.api import API
 
 class Vote(API):
     def vote(self, transaction):
-    return self.client.transport().createTransaction(transaction)
+        return self.client.transport().createTransaction(transaction)
 
     def unvote(self, transaction):
         return self.client.transport().createTransaction(transaction)
