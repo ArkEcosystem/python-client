3954050d351119872eb7f3cedf2f38e1a81eb446
galperins4 <galperins4@gmail.com>
2018-06-14 22:55:47 -0500

    Add files via upload

--- park/one/delegate.py
+++ park/one/delegate.py
@@ -6,25 +6,25 @@ from park.one.api import API
 class Delegate(API):
 
     def count(self):
-        return self.get('api/'+self.client.api_version+'/delegates/count')
+        return self.get('api/delegates/count')
 
     def search(self, query, parameters={}):
-        return self.get('api/'+self.client.api_version+'/delegates/search', {**{"q": query}, **parameters})
+        return self.get('api/delegates/search', {**{"q": query}, **parameters})
 
     def voters(self, publicKey):
-        return self.get('api/'+self.client.api_version+'/delegates/voters', {"publicKey": publicKey})
+        return self.get('api/delegates/voters', {"publicKey": publicKey})
 
     def delegate(self, parameters={}):
-        return self.get('api/'+self.client.api_version+'/delegates/get', parameters)
+        return self.get('api/delegates/get', parameters)
 
     def delegates(self, parameters={}):
-        return self.get('api/'+self.client.api_version+'/delegates', parameters)
+        return self.get('api/delegates', parameters)
 
     def fee(self):
-        return self.get('api/'+self.client.api_version+'/delegates/fee')
+        return self.get('api/delegates/fee')
 
     def forgedByAccount(self, generatorPublicKey):
-        return self.get('api/'+self.client.api_version+'/delegates/forging/getForgedByAccount',
+        return self.get('api/delegates/forging/getForgedByAccount',
                         {"generatorPublicKey": generatorPublicKey})
 
     def create(self, secret, username, secondSecret=None):
@@ -34,10 +34,10 @@ class Delegate(API):
         return self.client.transport().createTransaction(transaction)
 
     def nextForgers(self):
-        return self.get('api/'+self.client.api_version+'/delegates/getNextForgers')
+        return self.get('api/delegates/getNextForgers')
 
     def enableForging(self, secret, parameters={}):
-        return self.post('api/'+self.client.api_version+'/delegates/forging/enable', {
+        return self.post('api/delegates/forging/enable', {
             **{
                 "secret": secret
             },
@@ -45,7 +45,7 @@ class Delegate(API):
         })
 
     def disableForging(self, secret, parameters={}):
-        return self.post('api/'+self.client.api_version+'/delegates/forging/disable', {
+        return self.post('api/delegates/forging/disable', {
             **{
                 "secret": secret
             },
@@ -53,7 +53,7 @@ class Delegate(API):
         })
 
     def forgingStatus(self, publicKey, parameters={}):
-        return self.get('api/'+self.client.api_version+'/delegates/forging/status', {
+        return self.get('api/delegates/forging/status', {
             **{
                 "publicKey": publicKey
             },
