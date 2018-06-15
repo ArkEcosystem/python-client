563ed5bbe846cd5181b833d185b16611a77ce095
galperins4 <galperins4@gmail.com>
2018-06-14 22:40:43 -0500

    Add files via upload

--- park/one/account.py
+++ park/one/account.py
@@ -4,32 +4,25 @@ from park.one.api import API
 
 class Account(API):
     def balance(self, address):
-        return self.get('api/'+self.client.api_version+'/accounts/getBalance', {"address": address})
+        return self.get('api/accounts/getBalance', {"address": address})
 
     def publickey(self, address):
-        return self.get('api/'+self.client.api_version+'/accounts/getPublicKey', {"address": address})
+        return self.get('api/accounts/getPublicKey', {"address": address})
 
     def delegates(self, address):
-        return self.get('api/'+self.client.api_version+'/accounts/delegates', {"address": address})
+        return self.get('api/accounts/delegates', {"address": address})
 
     def delegatesFee(self):
-        return self.get('api/' + self.client.api_version + '/accounts/delegates/fee')
-
-    def vote(self, secret, publicKey, secondSecret):
-        return self.put('api/'+self.client.api_version+'/accounts/delegates', {
-            "secret": secret,
-            "publicKey": publicKey,
-            "secondSecret": secondSecret
-        })
+        return self.get('api/accounts/delegates/fee')
 
     def account(self, address):
-        return self.get('api/'+self.client.api_version+'/accounts', {"address": address})
+        return self.get('api/accounts', {"address": address})
 
     def accounts(self, parameters={}):
-        return self.get('api/'+self.client.api_version+'/accounts/getAllAccounts', parameters)
+        return self.get('api/accounts/getAllAccounts', parameters)
 
     def top(self, parameters={}):
-        return self.get('api/'+self.client.api_version+'/accounts/top', parameters)
+        return self.get('api/accounts/top', parameters)
 
     def count(self):
-        return self.get('api/'+self.client.api_version+'/accounts/count')
+        return self.get('api/accounts/count')
