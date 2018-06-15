3e837aa2c40f00ee574f8d691747a0f74894d20d
galperins4 <galperins4@gmail.com>
2018-06-14 22:47:33 -0500

    Add files via upload

--- park/one/block.py
+++ park/one/block.py
@@ -5,34 +5,34 @@ from park.one.api import API
 
 class Block(API):
     def block(self, id):
-        return self.get('api/'+self.client.api_version+'/blocks/get', {"id": id})
+        return self.get('api/blocks/get', {"id": id})
 
     def blocks(self, parameters={}):
-        return self.get('api/'+self.client.api_version+'/blocks', parameters)
+        return self.get('api/blocks', parameters)
 
     def epoch(self):
-        return self.get('api/'+self.client.api_version+'/blocks/getEpoch')
+        return self.get('api/blocks/getEpoch')
 
     def height(self):
-        return self.get('api/'+self.client.api_version+'/blocks/getHeight')
+        return self.get('api/blocks/getHeight')
 
     def nethash(self):
-        return self.get('api/'+self.client.api_version+'/blocks/getNethash')
+        return self.get('api/blocks/getNethash')
 
     def fee(self):
-        return self.get('api/'+self.client.api_version+'/blocks/getFee')
+        return self.get('api/blocks/getFee')
 
     def fees(self):
-        return self.get('api/'+self.client.api_version+'/blocks/getFees')
+        return self.get('api/blocks/getFees')
 
     def milestone(self):
-        return self.get('api/'+self.client.api_version+'/blocks/getMilestone')
+        return self.get('api/blocks/getMilestone')
 
     def reward(self):
-        return self.get('api/'+self.client.api_version+'/blocks/getReward')
+        return self.get('api/blocks/getReward')
 
     def supply(self):
-        return self.get('api/'+self.client.api_version+'/blocks/getSupply')
+        return self.get('api/blocks/getSupply')
 
     def status(self):
-        return self.get('api/'+self.client.api_version+'/blocks/getStatus')
+        return self.get('api/blocks/getStatus')
