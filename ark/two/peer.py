f63312b58b80c747f84f52a643751c50a28c49ef
galperins4 <galperins4@gmail.com>
2018-06-16 21:27:08 -0500

    Update peer.py

--- ark/two/peer.py
+++ ark/two/peer.py
@@ -4,7 +4,7 @@ from ark.two.api import API
 class Peer2(API):
 
     def peer(self, ip):
-        return self.get(f'api/v2/peers/{ip}')
+        return self.get('api/v2/peers/{0}'.format(ip))
 
     def peers(self, parameters=None):
         return self.get('api/v2/peers', parameters)
