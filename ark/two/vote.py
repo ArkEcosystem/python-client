6466a460ed6b768b242badcc6206b268663e89fa
galperins4 <galperins4@gmail.com>
2018-06-16 21:28:42 -0500

    Update vote.py

--- ark/two/vote.py
+++ ark/two/vote.py
@@ -4,7 +4,7 @@ from ark.two.api import API
 class Vote2(API):
 
     def vote(self, id):
-        return self.get(f'api/v2/votes/{id}')
+        return self.get('api/v2/votes/{0}'.format(id))
 
     def votes(self, parameters=None):
         return self.get('api/v2/votes', parameters)
