6681deff18755b008b47caf5f2113c5f7684492c
galperins4 <galperins4@gmail.com>
2018-06-16 21:28:21 -0500

    Update transaction.py

--- ark/two/transaction.py
+++ ark/two/transaction.py
@@ -7,10 +7,10 @@ class Transaction2(API):
         raise NotImplementedError()
 
     def transaction(self, id):
-        return self.get(f'api/v2/transactions/{id}')
+        return self.get('api/v2/transactions/{0}'.format(id))
 
     def unconfirmedTransaction(self, id):
-        return self.get(f'api/v2/transactions/unconfirmed/{id}')
+        return self.get('api/v2/transactions/unconfirmed/{0}'.format(id))
 
     def transactions(self, parameters=None):
         return self.get('api/v2/transactions', parameters)
