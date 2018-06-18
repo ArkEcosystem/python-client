3b3cde5186f4afccef331952b6ec0b7e140ae5c3
galperins4 <galperins4@gmail.com>
2018-06-17 21:33:52 -0500

    snake_case update

--- ark/two/p2p.py
+++ ark/two/p2p.py
@@ -6,25 +6,22 @@ class P2p(API):
     def list(self):
         return self.get('peer/list')
 
-    def blocksCommon(self, ids):
+    def blocks_common(self, ids):
         return self.get('peer/blocks/common', {'ids': ','.join(ids)})
 
     def blocks(self):
         return self.get('peer/blocks')
 
-    def createBlock(self, block):
+    def create_block(self, block):
         return self.post('peer/blocks', {'block': block})
 
     def transactions(self):
         return self.get('peer/transactions')
 
-    def transactionsFromIds(self, ids):
+    def transactions_from_ids(self, ids):
         return self.get('peer/transactionsFromIds', {'ids': ','.join(ids)})
 
-    def createTransaction(self, transaction):
-        return self.post('peer/transactions', {'transactions': [transaction]})
-
-    def createBatchTransaction(self, transaction):
+    def create_transaction(self, transaction):
         return self.post('peer/transactions', {'transactions': transaction})
 
     def height(self):
@@ -32,13 +29,3 @@ class P2p(API):
 
     def status(self):
         return self.get('peer/status')
-
-
-
-
-
-
-
-
-
-
