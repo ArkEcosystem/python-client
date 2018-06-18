ef78c2d0181af8863452455b022f9646d0beec3b
galperins4 <galperins4@gmail.com>
2018-06-17 21:31:34 -0500

    snake_case update

--- ark/two/block.py
+++ ark/two/block.py
@@ -8,8 +8,8 @@ class Block2(API):
     def block(self, id, parameters=None):
         return self.get('api/v2/blocks/{0}'.format(id), parameters)
 
-    def blockTransactions(self, id, parameters=None):
+    def block_transactions(self, id, parameters=None):
         return self.get('api/v2/blocks/{0}/transactions'.format(id), parameters)
 
-    def blockSearch(self, parameters=None):
+    def block_search(self, parameters=None):
         raise NotImplementedError()
