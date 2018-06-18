01855f3ba0d6fbb1798c183b86fad03798c95196
Tomaž Šifrer <3967564+tsifrer@users.noreply.github.com>
2018-06-18 22:28:06 +0200

    Initial pytest integration (#15)

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+import sys
+
 import setuptools
+
 
 requires = [
     'requests>=2.19.1',
@@ -8,12 +11,15 @@ tests_require = [
     'flake8>=3.5.0',
     'flake8-import-order>=0.17.1',
     'flake8-quotes>=1.0.0',
+    'pytest>=3.6.1',
 ]
 
 extras_require = {
     'test': tests_require,
     'dev': requires + tests_require
 }
+
+setup_requires = ['pytest-runner'] if {'pytest', 'test', 'ptr'}.intersection(sys.argv) else []
 
 setuptools.setup(
     name='ark-client',
@@ -22,13 +28,9 @@ setuptools.setup(
     author='',
     author_email='',
     url='https://github.com/ArkEcosystem/python-client',
-    packages=[
-        'ark',
-        'ark.one',
-        'ark.two',
-        'ark.p2p'
-    ],
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
     install_requires=requires,
     extras_require=extras_require,
     tests_require=tests_require,
+    setup_requires=setup_requires,
 )
