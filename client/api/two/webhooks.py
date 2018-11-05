4104a8f328c33f48325036f890f1a88c1dad19fd
Thomas Neuman <neuman.tn@gmail.com>
2018-11-05 00:22:09 -0800

    feat: implement webhooks APIs

---
+++ client/api/two/webhooks.py
@@ -0,0 +1,48 @@
+from client.resource import Resource
+
+
+class Webhooks(Resource):
+
+    def all(self, page=None, limit=20):
+        params = {
+            'page': page,
+            'limit': limit
+        }
+        return self.request_get('webhooks', params)
+
+    def create(self, event, target, conditions, enabled=None):
+        data = {
+            'event': event,
+            'target': target,
+            'conditions': conditions
+        }
+        if enabled is not None:
+            data.update({
+                'enabled': enabled
+            })
+        return self.request_post('webhooks', data)
+
+    def get(self, webhook_id):
+        return self.request_get('webhooks/{}'.format(webhook_id))
+
+    def update(
+            self,
+            webhook_id,
+            event=None,
+            target=None,
+            conditions=None,
+            enabled=None
+    ):
+        data = {}
+        if event is not None:
+            data.update({'event': event})
+        if target is not None:
+            data.update({'target': target})
+        if conditions is not None:
+            data.update({'conditions': conditions})
+        if enabled is not None:
+            data.update({'enabled': enabled})
+        return self.request_put('webhooks/{}'.format(webhook_id), data=data)
+
+    def delete(self, webhook_id):
+        return self.request_delete('webhooks/{}'.format(webhook_id))
---
+++ tests/api/two/test_webhooks.py
@@ -0,0 +1,177 @@
+import json
+from urllib.parse import urlencode
+
+import responses
+
+from client import ArkClient
+
+
+def test_all_calls_correct_url_with_default_params():
+    responses.add(
+        responses.GET,
+        'http://127.0.0.1:4002/webhooks',
+        json={'success': True},
+        status=200
+    )
+
+    client = ArkClient('http://127.0.0.1:4002', api_version='v2')
+    client.webhooks.all()
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == 'http://127.0.0.1:4002/webhooks?limit=20'
+
+
+def test_all_calls_correct_url_with_passed_in_params():
+    responses.add(
+        responses.GET,
+        'http://127.0.0.1:4002/webhooks',
+        json={'success': True},
+        status=200
+    )
+
+    client = ArkClient('http://127.0.0.1:4002', api_version='v2')
+    client.webhooks.all(page=5, limit=69)
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url.startswith('http://127.0.0.1:4002/webhooks?')
+    assert 'page=5' in responses.calls[0].request.url
+    assert 'limit=69' in responses.calls[0].request.url
+
+
+def test_get_calls_correct_url():
+    webhook_id = '12345'
+    responses.add(
+        responses.GET,
+        'http://127.0.0.1:4002/webhooks/{}'.format(webhook_id),
+        json={'success': True},
+        status=200
+    )
+
+    client = ArkClient('http://127.0.0.1:4002', api_version='v2')
+    client.webhooks.get(webhook_id)
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == 'http://127.0.0.1:4002/webhooks/12345'
+
+
+def test_create_calls_correct_url_with_no_enabled_param():
+    event = 'event'
+    target = 'target'
+    conditions = []
+    responses.add(
+        responses.POST,
+        'http://127.0.0.1:4002/webhooks',
+        json={'success': True},
+        status=201
+    )
+
+    client = ArkClient('http://127.0.0.1:4002', api_version='v2')
+    client.webhooks.create(event, target, conditions)
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == 'http://127.0.0.1:4002/webhooks'
+    assert json.loads(responses.calls[0].request.body.decode()) == \
+            {'event': event, 'target': target, 'conditions': conditions}
+
+
+def test_create_calls_correct_url_with_enabled_param():
+    event = 'event'
+    target = 'target'
+    conditions = []
+    enabled = True
+    responses.add(
+        responses.POST,
+        'http://127.0.0.1:4002/webhooks',
+        json={'success': True},
+        status=201
+    )
+
+    client = ArkClient('http://127.0.0.1:4002', api_version='v2')
+    client.webhooks.create(event, target, conditions, enabled=enabled)
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == 'http://127.0.0.1:4002/webhooks'
+    assert json.loads(responses.calls[0].request.body.decode()) == \
+            {'event': event, 'target': target, 'conditions': conditions, 'enabled': enabled}
+
+
+def test_update_calls_correct_url_with_event_param():
+    webhook_id = '12345'
+    event = 'event'
+    responses.add(
+        responses.PUT,
+        'http://127.0.0.1:4002/webhooks/{}'.format(webhook_id),
+        json={'success': True},
+        status=204
+    )
+
+    client = ArkClient('http://127.0.0.1:4002', api_version='v2')
+    client.webhooks.update(webhook_id, event=event)
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == 'http://127.0.0.1:4002/webhooks/12345'
+    assert json.loads(responses.calls[0].request.body.decode()) == \
+            {'event': event}
+
+
+def test_update_calls_correct_url_with_target_param():
+    webhook_id = '12345'
+    target = 'target'
+    responses.add(
+        responses.PUT,
+        'http://127.0.0.1:4002/webhooks/{}'.format(webhook_id),
+        json={'success': True},
+        status=204
+    )
+
+    client = ArkClient('http://127.0.0.1:4002', api_version='v2')
+    client.webhooks.update(webhook_id, target=target)
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == 'http://127.0.0.1:4002/webhooks/12345'
+    assert json.loads(responses.calls[0].request.body.decode()) == \
+            {'target': target}
+
+
+def test_update_calls_correct_url_with_conditions_param():
+    webhook_id = '12345'
+    conditions = []
+    responses.add(
+        responses.PUT,
+        'http://127.0.0.1:4002/webhooks/{}'.format(webhook_id),
+        json={'success': True},
+        status=204
+    )
+
+    client = ArkClient('http://127.0.0.1:4002', api_version='v2')
+    client.webhooks.update(webhook_id, conditions=conditions)
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == 'http://127.0.0.1:4002/webhooks/12345'
+    assert json.loads(responses.calls[0].request.body.decode()) == \
+            {'conditions': conditions}
+
+
+def test_update_calls_correct_url_with_enabled_param():
+    webhook_id = '12345'
+    enabled = True
+    responses.add(
+        responses.PUT,
+        'http://127.0.0.1:4002/webhooks/{}'.format(webhook_id),
+        json={'success': True},
+        status=204
+    )
+
+    client = ArkClient('http://127.0.0.1:4002', api_version='v2')
+    client.webhooks.update(webhook_id, enabled=enabled)
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == 'http://127.0.0.1:4002/webhooks/12345'
+    assert json.loads(responses.calls[0].request.body.decode()) == \
+            {'enabled': enabled}
+
+
+def test_delete_calls_correct_url():
+    webhook_id = '12345'
+    responses.add(
+        responses.DELETE,
+        'http://127.0.0.1:4002/webhooks/{}'.format(webhook_id),
+        json={'success': True},
+        status=204
+    )
+
+    client = ArkClient('http://127.0.0.1:4002', api_version='v2')
+    client.webhooks.delete(webhook_id)
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == 'http://127.0.0.1:4002/webhooks/12345'
