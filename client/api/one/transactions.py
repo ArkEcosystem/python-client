0f5d50c6ab9584a97b5bda672bb858b6ef1b6be4
highjhacker <mc-somango@live.fr>
2018-11-18 21:40:08 +0100

    Every query parameters of the transactions endpoint are now working. Added some tests for that new feature.

--- client/api/one/transactions.py
+++ client/api/one/transactions.py
@@ -7,12 +7,14 @@ class Transactions(Resource):
     def get(self, transaction_id):
         return self.request_get('transactions/get', {'id': transaction_id})
 
-    def all(self, limit=20, offset=None):
+    def all(self, limit=20, offset=None, **kwargs):
         if limit > 100:
             raise ArkParameterException('Maximum number of objects to return is 100')
+        extra_params = {name: kwargs[name] for name in kwargs if kwargs[name] is not None}
         params = {
             'limit': limit,
             'offset': offset,
+            **extra_params
         }
         return self.request_get('transactions', params)
 
