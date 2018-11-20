50936e82609a35074f4561cb7d82f657a14f1409
highjhacker <mc-somango@live.fr>
2018-11-20 13:48:33 +0100

    Fix indentation for PEP8

--- client/api/one/transactions.py
+++ client/api/one/transactions.py
@@ -7,8 +7,9 @@ class Transactions(Resource):
     def get(self, transaction_id):
         return self.request_get('transactions/get', {'id': transaction_id})
 
-    def all(self, limit=20, offset=None, block_id=None, type=None, order_by=None, sender_public_key=None, vendor_field=None,
-            owner_public_key=None, owner_address=None, sender_id=None, recipient_id=None, amount=None, fee=None):
+    def all(self, limit=20, offset=None, block_id=None, type=None, order_by=None, sender_public_key=None,
+            vendor_field=None, owner_public_key=None, owner_address=None, sender_id=None, recipient_id=None,
+            amount=None, fee=None):
         if limit > 100:
             raise ArkParameterException('Maximum number of objects to return is 100')
         params = {
