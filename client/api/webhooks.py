7c1f8a7314a34463b702de6aeb25938fe4784d83
galperins4 <galperins4@gmail.com>
2018-12-28 22:21:58 -0600

    Add files via upload

--- client/api/webhooks.py
+++ client/api/webhooks.py
@@ -3,16 +3,13 @@ from client.resource import Resource
 
 class Webhooks(Resource):
 
-
     def __init__(self):
         self.events = []
         self.conditions = []
         #pass - need to swap ports for call to work
 
-
     def test(self):
         print(self)
-
 
     def get(self, page=None, limit=100):
         params = {
@@ -21,17 +18,14 @@ class Webhooks(Resource):
         }
         return self.request_get('webhooks', params)
 
-
     def retrieve(self, webhook_id):
         return self.request_get('webhooks/{}'.format(webhook_id))
-
 
     def create(self, event, target, conditions, enabled=None):
         return self.request_post('webhooks', data={'event': event,
                                                    'target': target,
                                                    'conditions': conditions,
                                                    'enabled': enabled})
-
 
     def update(self, webhook_id, event=None, target=None, conditions=None, enabled=None):
         return self.request_put('webhooks/{}'.format(webhook_id),
@@ -40,8 +34,5 @@ class Webhooks(Resource):
                                       'conditions': conditions,
                                       'enabled': enabled})
 
-
     def delete(self, webhook_id):
         return self.request_delete('webhooks/{}'.format(webhook_id))
-
-
