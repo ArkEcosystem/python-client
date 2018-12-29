334b22b197a236cf5706df45bc5fdb5b2dceab2c
galperins4 <galperins4@gmail.com>
2018-12-29 00:23:08 -0600

    Update webhooks.py

--- client/api/webhooks.py
+++ client/api/webhooks.py
@@ -1,31 +1,9 @@
-from client.connection import Connection
 from client.resource import Resource
-from dotenv import load_dotenv
-import os
-from pathlib import Path
 
 
 class Webhooks(Resource):
-
-    def swap_ports(self):
-        env = str(Path.home()) + '/.ark/.env'
-        if os.path.exists(env) is True:
-            load_dotenv(env)
-            api = os.getenv("ARK_API_PORT") 
-            webhook = os.getenv("ARK_WEBHOOKS_PORT")
-        else:
-            api = "4003"
-            webhook = "4004"
-
-        #reinstiate Connection with replaced port
-        print(self.connection.hostname)
-        newhost = self.connection.hostname.replace(api,webhook)
-        print(newhost)
-        Connection(newhost)
         
     def get(self, page=None, limit=100):
-        self.swap_ports()
-        print(self.connection.hostname)
         params = {
             'page': page,
             'limit': limit,
@@ -33,7 +11,6 @@ class Webhooks(Resource):
         return self.request_get('webhooks', params)
 
     def retrieve(self, webhook_id):
-        self.swap_ports()
         return self.request_get('webhooks/{}'.format(webhook_id))
 
     def create(self, event, target, conditions, enabled=None):
@@ -44,7 +21,6 @@ class Webhooks(Resource):
                                                    'enabled': enabled})
 
     def update(self, webhook_id, event=None, target=None, conditions=None, enabled=None):
-        self.swap_ports()
         return self.request_put('webhooks/{}'.format(webhook_id),
                                 data={'event': event,
                                       'target': target,
@@ -52,5 +28,4 @@ class Webhooks(Resource):
                                       'enabled': enabled})
 
     def delete(self, webhook_id):
-        self.swap_ports()
         return self.request_delete('webhooks/{}'.format(webhook_id))
