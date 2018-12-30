415176a7318377a5acf719552343ec78eec31ef4
galperins4 <galperins4@gmail.com>
2018-12-29 21:44:05 -0600

    Update test_webhooks.py

--- tests/api/test_webhooks.py
+++ tests/api/test_webhooks.py
@@ -8,50 +8,50 @@ from client import ArkClient
 def test_get_calls_correct_url_with_default_params():
     responses.add(
         responses.GET,
-        'http://127.0.0.1:4002/delegates',
+        'http://127.0.0.1:4004/webhooks',
         json={'success': True},
         status=200
     )
 
-    client = ArkClient('http://127.0.0.1:4002')
-    client.delegates.all()
+    client = ArkClient('http://127.0.0.1:4004')
+    client.webhooks.get()
     assert len(responses.calls) == 1
-    assert responses.calls[0].request.url == 'http://127.0.0.1:4002/delegates?limit=100'
+    assert responses.calls[0].request.url == 'http://127.0.0.1:4004/webhooks?limit=100'
 
 
 def test_get_calls_correct_url_with_passed_in_params():
     responses.add(
         responses.GET,
-        'http://127.0.0.1:4002/delegates',
+        'http://127.0.0.1:4004/webhooks',
         json={'success': True},
         status=200
     )
 
-    client = ArkClient('http://127.0.0.1:4002')
-    client.delegates.all(page=5, limit=69)
+    client = ArkClient('http://127.0.0.1:4004')
+    client.webhooks.get(page=5, limit=69)
     assert len(responses.calls) == 1
-    assert responses.calls[0].request.url.startswith('http://127.0.0.1:4002/delegates?')
+    assert responses.calls[0].request.url.startswith('http://127.0.0.1:4004/webhooks?')
     assert 'page=5' in responses.calls[0].request.url
     assert 'limit=69' in responses.calls[0].request.url
 
 
 def test_retrieve_calls_correct_url():
-    delegate_id = '12345'
+    webhook_id = 1
     responses.add(
         responses.GET,
-        'http://127.0.0.1:4002/delegates/{}'.format(delegate_id),
+        'http://127.0.0.1:4004/webhooks/{}'.format(webhook_id),
         json={'success': True},
         status=200
     )
 
-    client = ArkClient('http://127.0.0.1:4002')
-    client.delegates.get(delegate_id)
+    client = ArkClient('http://127.0.0.1:4004')
+    client.webhook.retrieve(webhook_id)
 
     assert len(responses.calls) == 1
-    assert responses.calls[0].request.url == 'http://127.0.0.1:4002/delegates/12345'
+    assert responses.calls[0].request.url == 'http://127.0.0.1:4004/webhooks/1'
 
 ##### ADD CREATE TESTS
-    
+##### UPDATE PUT REQUESTS
 def test_update_calls_correct_url_with_default_params():
     delegate_id = '12345'
     responses.add(
@@ -89,16 +89,16 @@ def test_update_calls_correct_url_with_passed_in_params():
 
 
 def test_delete_calls_correct_url():
-    delegate_id = '12345'
+    webhook_id = '1'
     responses.add(
         responses.GET,
-        'http://127.0.0.1:4002/delegates/{}'.format(delegate_id),
+        'http://127.0.0.1:4004/webhooks/{}'.format(webhook_id),
         json={'success': True},
         status=200
     )
 
-    client = ArkClient('http://127.0.0.1:4002')
-    client.delegates.get(delegate_id)
+    client = ArkClient('http://127.0.0.1:4004')
+    client.webhooks.delete(webhook_id)
 
     assert len(responses.calls) == 1
-    assert responses.calls[0].request.url == 'http://127.0.0.1:4002/delegates/12345'
+    assert responses.calls[0].request.url == 'http://127.0.0.1:4004/webhooks/1'
