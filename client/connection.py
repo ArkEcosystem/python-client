dd9157267790cb7e289291d6bf81047e89ee2720
galperins4 <galperins4@gmail.com>
2018-12-29 19:49:34 -0600

    Update connection.py

--- client/connection.py
+++ client/connection.py
@@ -49,7 +49,7 @@ class Connection(object):
 
     def _handle_response(self, response):
         if not response.content:
-            print("response.content")
+            print(response.content)
             raise ArkHTTPException('No content in response', response=response)
 
         body = response.json()
