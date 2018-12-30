3b985d22bb114cde3f102ffc3dbe14b10350d669
galperins4 <galperins4@gmail.com>
2018-12-29 22:06:37 -0600

    Update test_webhooks.py

--- tests/api/test_webhooks.py
+++ tests/api/test_webhooks.py
@@ -65,7 +65,7 @@ def test_create_calls_correct_url_with_passed_in_params():
     assert len(responses.calls) == 1
     assert responses.calls[0].request.url.startswith('http://127.0.0.1:4004/api/webhooks?')
     assert json.loads(responses.calls[0].request.body.decode()) == {
-        'event' : 'block.forged', 'target : '127.0.0.1:5000', 
+        'event' : 'block.forged', 'target' : '127.0.0.1:5000', 
         'conditions' : [{'random':'data'}], 'enabled':'true'}
 
 
@@ -84,7 +84,7 @@ def test_update_calls_correct_url_with_passed_in_params():
     assert len(responses.calls) == 1
     assert responses.calls[0].request.url.startswith('http://127.0.0.1:4004/api/webhooks/1?')
     assert json.loads(responses.calls[0].request.body.decode()) == {
-        'event' : 'block.forged', 'target : '127.0.0.1:5000', 
+        'event' : 'block.forged', 'target' : '127.0.0.1:5000', 
         'conditions' : [{'random':'data'}], 'enabled':'false'}
 
 
