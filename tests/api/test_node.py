79a86cac39a1ea18ef91db4faef14f896c78019f
Edgar Goetzendorff <hello@dated.fun>
2019-05-09 16:36:12 +0200

    feat: add node/fees endpoint

--- tests/api/test_node.py
+++ tests/api/test_node.py
@@ -43,3 +43,32 @@ def test_configuration_calls_correct_url():
     client.node.configuration()
     assert len(responses.calls) == 1
     assert responses.calls[0].request.url == 'http://127.0.0.1:4002/node/configuration'
+
+
+def test_fees_calls_correct_url():
+    responses.add(
+        responses.GET,
+        'http://127.0.0.1:4002/node/fees',
+        json={'success': True},
+        status=200
+    )
+
+    client = ArkClient('http://127.0.0.1:4002')
+    client.node.fees()
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == 'http://127.0.0.1:4002/node/fees'
+
+
+def test_fees_calls_correct_url_with_passed_in_params():
+    responses.add(
+        responses.GET,
+        'http://127.0.0.1:4002/node/fees',
+        json={'success': True},
+        status=200
+    )
+
+    client = ArkClient('http://127.0.0.1:4002')
+    client.node.fees(days=14)
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url.startswith('http://127.0.0.1:4002/node/fees?')
+    assert 'days=14' in responses.calls[0].request.url
