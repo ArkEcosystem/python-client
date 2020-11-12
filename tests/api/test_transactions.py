4c057bf4a5648a07b8551a07a51da23933dc67b1
Highjhacker <jolanbeer@gmail.com>
2020-11-12 13:44:22 +0100

    refactor: removal of dead endpoints

--- tests/api/test_transactions.py
+++ tests/api/test_transactions.py
@@ -136,38 +136,6 @@ def test_all_unconfirmed_calls_correct_url_with_additional_params():
     assert 'orderBy=timestamp.epoch' in responses.calls[0].request.url
 
 
-def test_search_calls_correct_url_with_default_params():
-    responses.add(
-        responses.POST,
-        'http://127.0.0.1:4002/transactions/search',
-        json={'success': True},
-        status=200
-    )
-
-    client = ArkClient('http://127.0.0.1:4002')
-    client.transactions.search({'blockId': '1337'})
-    assert len(responses.calls) == 1
-    assert responses.calls[0].request.url == 'http://127.0.0.1:4002/transactions/search?limit=100'
-    assert json.loads(responses.calls[0].request.body.decode()) == {'blockId': '1337'}
-
-
-def test_search_calls_correct_url_with_passed_in_params():
-    responses.add(
-        responses.POST,
-        'http://127.0.0.1:4002/transactions/search',
-        json={'success': True},
-        status=200
-    )
-
-    client = ArkClient('http://127.0.0.1:4002')
-    client.transactions.search({'blockId': '1337'}, page=5, limit=69)
-    assert len(responses.calls) == 1
-    assert responses.calls[0].request.url.startswith('http://127.0.0.1:4002/transactions/search?')
-    assert 'page=5' in responses.calls[0].request.url
-    assert 'limit=69' in responses.calls[0].request.url
-    assert json.loads(responses.calls[0].request.body.decode()) == {'blockId': '1337'}
-
-
 def test_transaction_types():
     responses.add(
         responses.GET,
