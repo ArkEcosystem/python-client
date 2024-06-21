2c4adb036fdce681e288357a6e10c0614012c72a
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-06-21 14:59:27 -0600

    add blockchain class

---
+++ tests/api/test_blockchain.py
@@ -0,0 +1,17 @@
+import responses
+from client import ArkClient
+
+
+@responses.activate
+def test_blockchain_calls_correct_url():
+    responses.add(
+        responses.GET,
+        'http://127.0.0.1:4002/blockchain',
+        json={'success': True},
+        status=200
+    )
+
+    client = ArkClient('http://127.0.0.1:4002')
+    client.blockchain.blockchain()
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == 'http://127.0.0.1:4002/blockchain'
