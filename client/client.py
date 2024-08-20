a6ddced587782911ab5a655757c8e073fb07743c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-08-20 14:01:32 +0100

    refactor: client api endpoint methods (#126)

--- client/client.py
+++ client/client.py
@@ -1,11 +1,15 @@
-import inspect
-import pkgutil
-from importlib import import_module
-from pathlib import Path
-
 from client.connection import Connection
-from client.exceptions import ArkParameterException
-from client.resource import Resource
+from client.api.api_nodes import ApiNodes
+from client.api.blockchain import Blockchain
+from client.api.blocks import Blocks
+from client.api.commits import Commits
+from client.api.delegates import Delegates
+from client.api.node import Node
+from client.api.peers import Peers
+from client.api.rounds import Rounds
+from client.api.transactions import Transactions
+from client.api.votes import Votes
+from client.api.wallets import Wallets
 
 class ArkClient(object):
 
@@ -16,22 +20,91 @@ class ArkClient(object):
             on whatever url they want.
         """
         self.connection = Connection(hostname)
-        self._import_api()
 
-    def _import_api(self):
+    @property
+    def api_nodes(self):
         """
-        Dynamically imports API endpoints.
+        :return: Nodes API
+        :rtype: client.api.api_nodes.ApiNodes
         """
-        modules = pkgutil.iter_modules([str(Path(__file__).parent / 'api')])
-        for _, name, _ in modules:
-            module = import_module('client.api.{}'.format(name))
-            for attr in dir(module):
-                # If attr name is `Resource`, skip it as it's a class and also has a
-                # subclass of Resource
-                if attr == 'Resource':
-                    continue
+        return ApiNodes(self.connection)
 
-                attribute = getattr(module, attr)
-                if inspect.isclass(attribute) and issubclass(attribute, Resource):
-                    # Set module class as a property on the client
-                    setattr(self, name, attribute(self.connection))
+    @property
+    def blockchain(self):
+        """
+        :return: Blockchain API
+        :rtype: client.api.blockchain.Blockchain
+        """
+        return Blockchain(self.connection)
+
+    @property
+    def blocks(self):
+        """
+        :return: Blocks API
+        :rtype: client.api.blocks.Blocks
+        """
+        return Blocks(self.connection)
+
+    @property
+    def commits(self):
+        """
+        :return: Commits API
+        :rtype: client.api.commits.Commits
+        """
+        return Commits(self.connection)
+
+    @property
+    def delegates(self):
+        """
+        :return: Delegates API
+        :rtype: client.api.delegates.Delegates
+        """
+        return Delegates(self.connection)
+
+    @property
+    def node(self):
+        """
+        :return: Node API
+        :rtype: client.api.node.Node
+        """
+        return Node(self.connection)
+
+    @property
+    def peers(self):
+        """
+        :return: Peers API
+        :rtype: client.api.peers.Peers
+        """
+        return Peers(self.connection)
+
+    @property
+    def rounds(self):
+        """
+        :return: Rounds API
+        :rtype: client.api.rounds.Rounds
+        """
+        return Rounds(self.connection)
+
+    @property
+    def transactions(self):
+        """
+        :return: Transactions API
+        :rtype: client.api.transactions.Transactions
+        """
+        return Transactions(self.connection)
+
+    @property
+    def votes(self):
+        """
+        :return: Votes API
+        :rtype: client.api.votes.Votes
+        """
+        return Votes(self.connection)
+
+    @property
+    def wallets(self):
+        """
+        :return: Wallets API
+        :rtype: client.api.wallets.Wallets
+        """
+        return Wallets(self.connection)
