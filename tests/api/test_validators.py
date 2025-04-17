f9e0f513a49d2ee13690e8863a4be0fdeba5b050
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-04-17 15:58:18 +0100

    delegate test updates

--- tests/api/test_validators.py
+++ tests/api/test_validators.py
@@ -53,32 +53,32 @@ def test_all_calls_correct_url_with_additional_params():
 
 
 def test_get_calls_correct_url():
-    delegate_id = '12345'
+    validator_id = '12345'
     responses.add(
         responses.GET,
-        'http://127.0.0.1:4002/api/validators/{}'.format(delegate_id),
+        'http://127.0.0.1:4002/api/validators/{}'.format(validator_id),
         json={'success': True},
         status=200
     )
 
     client = ArkClient('http://127.0.0.1:4002/api')
-    client.validators.get(delegate_id)
+    client.validators.get(validator_id)
 
     assert len(responses.calls) == 1
     assert responses.calls[0].request.url == 'http://127.0.0.1:4002/api/validators/12345'
 
 
 def test_blocks_calls_correct_url():
-    delegate_id = '12345'
+    validator_id = '12345'
     responses.add(
         responses.GET,
-        'http://127.0.0.1:4002/api/validators/{}/blocks'.format(delegate_id),
+        'http://127.0.0.1:4002/api/validators/{}/blocks'.format(validator_id),
         json={'success': True},
         status=200
     )
 
     client = ArkClient('http://127.0.0.1:4002/api')
-    client.validators.blocks(delegate_id, limit=100, orderBy='timestamp:desc')
+    client.validators.blocks(validator_id, limit=100, orderBy='timestamp:desc')
 
     assert len(responses.calls) == 1
     assert responses.calls[0].request.url.startswith('http://127.0.0.1:4002/api/validators/12345/blocks?')
@@ -87,16 +87,16 @@ def test_blocks_calls_correct_url():
 
 
 def test_voters_calls_correct_url():
-    delegate_id = '12345'
+    validator_id = '12345'
     responses.add(
         responses.GET,
-        'http://127.0.0.1:4002/api/validators/{}/voters'.format(delegate_id),
+        'http://127.0.0.1:4002/api/validators/{}/voters'.format(validator_id),
         json={'success': True},
         status=200
     )
 
     client = ArkClient('http://127.0.0.1:4002/api')
-    client.validators.voters(delegate_id, limit=100, orderBy='timestamp:desc')
+    client.validators.voters(validator_id, limit=100, orderBy='timestamp:desc')
 
     assert len(responses.calls) == 1
     assert responses.calls[0].request.url.startswith('http://127.0.0.1:4002/api/validators/12345/voters?')
