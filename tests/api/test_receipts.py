a5541d1c50e84af8ffda4fa1512202819156d261
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-05-20 16:05:33 +0100

    fix: receipts get url (#138)

--- tests/api/test_receipts.py
+++ tests/api/test_receipts.py
@@ -36,7 +36,7 @@ def test_get_calls_correct_url():
     transaction_hash = '12345'
     responses.add(
         responses.GET,
-        f'http://127.0.0.1:4002/api/receipts?txHash={transaction_hash}',
+        f'http://127.0.0.1:4002/api/receipts/{transaction_hash}',
         json={'success': True},
         status=200
     )
@@ -44,5 +44,4 @@ def test_get_calls_correct_url():
     client = ArkClient('http://127.0.0.1:4002/api')
     client.receipts.get(transaction_hash)
     assert len(responses.calls) == 1
-    assert responses.calls[0].request.url.startswith('http://127.0.0.1:4002/api/receipts?')
-    assert f'txHash={transaction_hash}' in responses.calls[0].request.url
+    assert responses.calls[0].request.url == f'http://127.0.0.1:4002/api/receipts/{transaction_hash}'
